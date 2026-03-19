fe93298435ee0dcb9be8e99f8871fedc9f391fea
antable93 <adevergillo@gmail.com>
2026-03-19 18:28:07 -0400

    3

---
+++ .build/obj_ploopyco_pavonis_via/src/info_deps.d
@@ -0,0 +1,50 @@
+generated-files: $(wildcard keyboards/ploopyco/pavonis/info.json)
+
+generated-files: $(wildcard keyboards/ploopyco/pavonis/keyboard.json)
+
+generated-files: $(wildcard keyboards/ploopyco/pavonis/rules.mk)
+
+generated-files: $(wildcard keyboards/ploopyco/pavonis/post_rules.mk)
+
+generated-files: $(wildcard keyboards/ploopyco/pavonis/config.h)
+
+generated-files: $(wildcard keyboards/ploopyco/pavonis/post_config.h)
+
+generated-files: $(wildcard keyboards/ploopyco/info.json)
+
+generated-files: $(wildcard keyboards/ploopyco/keyboard.json)
+
+generated-files: $(wildcard keyboards/ploopyco/rules.mk)
+
+generated-files: $(wildcard keyboards/ploopyco/post_rules.mk)
+
+generated-files: $(wildcard keyboards/ploopyco/config.h)
+
+generated-files: $(wildcard keyboards/ploopyco/post_config.h)
+
+generated-files: $(wildcard C:/Users/Shadow/source/repos/qmk_firmware-1/keyboards/ploopyco/pavonis/keymaps/via/keymap.json)
+
+generated-files: $(wildcard C:/Users/Shadow/source/repos/qmk_firmware-1/keyboards/ploopyco/pavonis/keymaps/via/info.json)
+
+generated-files: $(wildcard C:/Users/Shadow/source/repos/qmk_firmware-1/keyboards/ploopyco/pavonis/keymaps/via/keyboard.json)
+
+generated-files: $(wildcard C:/Users/Shadow/source/repos/qmk_firmware-1/keyboards/ploopyco/pavonis/keymaps/via/rules.mk)
+
+generated-files: $(wildcard C:/Users/Shadow/source/repos/qmk_firmware-1/keyboards/ploopyco/pavonis/keymaps/via/post_rules.mk)
+
+generated-files: $(wildcard C:/Users/Shadow/source/repos/qmk_firmware-1/keyboards/ploopyco/pavonis/keymaps/via/config.h)
+
+generated-files: $(wildcard C:/Users/Shadow/source/repos/qmk_firmware-1/keyboards/ploopyco/pavonis/keymaps/via/post_config.h)
+
+generated-files: $(wildcard users/via/info.json)
+
+generated-files: $(wildcard users/via/keyboard.json)
+
+generated-files: $(wildcard users/via/rules.mk)
+
+generated-files: $(wildcard users/via/post_rules.mk)
+
+generated-files: $(wildcard users/via/config.h)
+
+generated-files: $(wildcard users/via/post_config.h)
+
